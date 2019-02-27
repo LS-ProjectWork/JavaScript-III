91d4b0e9362055d14662b4d80dfdbd383819ab78
dpayton23 <darien.payton71@gmail.com>
2019-02-27 15:22:40 -0500

    explained principle 3

--- assignments/this.js
+++ assignments/this.js
@@ -3,7 +3,8 @@
 *
 * 1. When a function is in the global scope, the 'this' keyword will point to the window.
 * 2. When the "this" keyword is used inside an object, it points to that object.
-* 3. 
+* 3. When the "this" keyword is used on a constructor function, it is referring to the object that will be created
+and is returned by the constructor function.
 * 4. 
 *
 * write out a code example of each explanation above
